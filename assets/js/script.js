ecebdfb4da0cca483782a702b9f080e9a3260f1c
AlanHarszczyk <C00285418@setu.ie>
2025-03-26 09:42:40 +0000

    echoes of eira art

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,10 @@
-document.addEventListener("DOMContentLoaded", () => {
+document.addEventListener("DOMContentLoaded", () => 
+    {
     console.log("Portfolio website loaded successfully!");
 
-    // Example functionality: Change text on click
-    document.querySelector('.text-section').addEventListener('click', () => {
+   
+    document.querySelector('.text-section').addEventListener('click', () => 
+    {
         alert('Welcome to my portfolio!');
     });
 });
